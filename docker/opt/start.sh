149eef5b26bc48c04d8dc2c177c3dfde58b0d316
jorgedlcruz <jorgedlcruz@gmail.com>
2017-10-30 16:11:39 +0000

    Added rsyslog

--- docker/opt/start.sh
+++ docker/opt/start.sh
@@ -139,6 +139,9 @@ cd /opt/zimbra-install/zcs-* && ./install.sh -s < /opt/zimbra-install/installZim
 echo "Installing Zimbra Collaboration injecting the configuration"
 /opt/zimbra/libexec/zmsetup.pl -c /opt/zimbra-install/installZimbraScript
 
+su - zimbra -c 'zmcontrol restart'
+echo "You can access now to your Zimbra Collaboration Server"
+
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
 fi
